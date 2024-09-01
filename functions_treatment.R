1f8b395c4b01ec8063a5eba3ce105fb0a14b16b1
IanA <iannaianjos@gmail.com>
2024-09-01 17:51:07 -0300

    01_08_24 17 adicionando arquivo de funções para o tratamento e modificando arquivo de tratamento

---
+++ functions_treatment.R
@@ -0,0 +1,26 @@
+#install.packages("yaml") primeira vez ao executar o código
+library(yaml)
+
+# Funções de tratamento
+criar_yaml <- function() {
+  dados <- list(
+    bd = "valor1",
+    x = "valor3",
+    y = "valor5",
+    model = list(
+      modelo_1 = 0,
+      modelo_2 = 0
+    ),
+    lambda = "lambda",
+    plot = 0
+  )
+
+  yaml_string <- as.yaml(dados)
+
+  write(yaml_string, file = "meu_arquivo.yaml")
+}
+
+
+
+
+
--- treatment.R
+++ treatment.R
@@ -1,1 +1,20 @@
 # tratamento
+# Sempre que o usuário pedir uma requisição na ferramenta o yaml deve ser recriado;
+# O código deve tratar o yaml que chega de forma a o 'model.R' conseguir entender a requisição;
+# Já nesse código será puxado a função correta do modelo requisitado;
+
+
+# Bibliotecas autorais
+source("model.R")
+source("functions_treatment.R")
+
+# Bibliotecas não autorais
+
+
+# Lógica:
+criar_yaml()
+
+
+
+
+
