1492c1f43305c2a918098c76e7727646404d57bf
Pedro Cabrini <121508215+pedropc0627@users.noreply.github.com>
2024-09-02 07:58:22 -0300

    Summary
    02_09_2024_08h_esboço_das_partes_do_codigo

--- model.R
+++ model.R
@@ -2,19 +2,19 @@
 library(yaml)
 library(glmnet)
 
-config <- yaml::read_yaml("arq_de_config.yaml")
+config = yaml::read_yaml("arq_de_config.yaml")
 
 #dados já estão no dir
-dados <- read.csv(config$nomde_dos_dados)
+dados =read.csv(config$nomde_dos_dados)
 
-X <- as.matrix(dados[, config$var_preditoras])
-y <- dados[[config$var_resposta]]
+X = as.matrix(dados[, config$var_preditoras])
+y = dados[[config$var_resposta]]
 
 if (config$tipo_de_modelo == 'lm') {
-  modelo <- lm(as.formula(paste(config$var_resposta, "~",
+  modelo = lm(as.formula(paste(config$var_resposta, "~",
                paste(var_preditoras, collapse = "+"))), data = dados)
 } else if (config$tipo_de_modelo == 'lasso') {
-  modelo <- glmnet(X, y, alpha = config$lasso_params$alpha,
+  modelo = glmnet(X, y, alpha = config$lasso_params$alpha,
                    lambda = config$lasso_params$lambda)
 #podemos adicionar ifelse para mais modelos
 }
--- plot.R
+++ plot.R
@@ -1,1 +1,14 @@
-# plot Pedro.F 
+# plot Pedro.F
+
+dados = read.csv(config$dataset)
+preds_json = fromJSON("local_de_saida/predicoes.json")
+
+ggplot(data = dados, aes(x = preds_treino, y = dados[[config$response_var]])) +
+  geom_point() +
+  geom_vline(xintercept = preds_json, linetype = "dashed", color = "red") +
+  ggtitle("Predições vs Valores Observados") +
+  xlab("Predições") +
+  ylab("Valores Observados") +
+  theme_minimal()
+
+ggsave("saidas/grafico.pdf")
