cbacee6c700c92025210ceeb6b5f8b4e444bd615
ian <ian.anjos@onerf.com.br>
2024-09-02 14:48:43 -0300

    02_09_2024 14:48

--- config.R
+++ config.R
@@ -1,1 +1,9 @@
-# congiguracoes
+# Configurações de código
+
+modelos = "model.R"
+tratamento = "treatment.R"
+funcoes_tratamento = "treatment_functions.R"
+plot = "plot.R"
+
+pasta_input = "entradas"
+pasta_output = "saidas"
