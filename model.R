bc91439edc8b9b2feb74fdab7c15c508f0e92e2c
Pedro Cabrini <121508215+pedropc0627@users.noreply.github.com>
2024-09-02 22:06:35 -0300

    Update model.R
    02_09_2024_22_atualizando_o_model

--- model.R
+++ model.R
@@ -1,42 +1,47 @@
 # Bibliotecas:
 library(yaml)
 library(glmnet)
-
+library(assertthat)
 # Bibliotecas autorais:
 source("config.R")
 
 # Uma vez tendo o yaml definido, treina o modelo
 chamar_modelo = function() {
-	config = yaml::read_yaml(paste0(pasta_input, "/", arquivo_yaml))
-
-	dados = read.csv(config$bd)
-
-	X = as.matrix(dados[, config$var_preditoras])
-	y = dados[[config$var_resposta]]
-
-	modelo_funcao = function(x, y, dados) {
-	  regressao = paste0(y, "~", x)
-	  ajuste = lm(as.formula(regressao), data = dados)
-	  return(list(beta0 = ajuste$coef[1], beta1 = ajuste$coef[2]))
-	}
-
-	modelo_usado = config$tipo_modelo
-	if (modelo_usado == 'lm') {
-	  modelo = modelo_funcao(config$var_preditoras, config$var_resposta, dados)
-	} else if (modelo_usado == 'lasso') {
-	  modelo = glmnet(X, y, alpha = config$lasso_params$alpha,
-		              lambda = config$lasso_params$lambda)
-	} else {
-	  stop(paste("Modelo", config$tipo_modelo, "não é reconhecido. Use 'lm' ou 'lasso'."))
-	}
-	
-	return(modelo)
-}
-
+  config = yaml::read_yaml(paste0(pasta_input, "/", arquivo_yaml))
+  
+  dados = read.csv(config$bd)
+  
+  X = as.matrix(dados[, config$var_preditoras])
+  y = dados[[config$var_resposta]]
+  
+  modelo_funcao = function(x, y, dados) {
+    regressao = paste0(y, "~", x)
+    ajuste = lm(as.formula(regressao), data = dados)
+    return(list(beta0 = ajuste$coef[1], beta1 = ajuste$coef[2]))
+  }
+  
+  modelo_usado = config$tipo_modelo
+  
+  # Verifique se o tipo de modelo foi definido
+  assert_that(!is.null(modelo_usado) && modelo_usado != "", 
+              msg = "Não setou nenhum tipo de modelo. Por favor, defina o tipo de modelo no arquivo YAML.")
+  
+  if (modelo_usado == 'lm') {
+    modelo = modelo_funcao(config$var_preditoras, config$var_resposta, dados)
+  } else if (modelo_usado == 'lasso') {
+    # Verifique o número de variáveis preditoras
+    assert_that(length(config$var_preditoras) >= 2, 
+                msg = "Lasso precisa de pelo menos duas variáveis preditoras.")
+    
+    modelo = glmnet(X, y, alpha = config$lasso_params$alpha,
+                    lambda = config$lasso_params$lambda)
+  } else {
+    stop(paste("Modelo", config$tipo_modelo, "não é reconhecido. Use 'lm' ou 'lasso'."))
+  }}
+return(modelo)
 # Salvar o modelo
 salvar_modelo = function(modelo) { 
 	nome_modelo = paste0(pasta_output, "/modelo_",gsub(".yaml", "", arquivo_yaml), "_", modelo_usado,".rds")
 	saveRDS(modelo, nome_modelo)
 	print(paste0("Modelo salvo em ", pasta_output, " com o nome '", nome_modelo, "'"))
 }
-
