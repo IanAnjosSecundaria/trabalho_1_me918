44c7823887b7b4117a57180615892a1a2f5d2fb0
IanA <iannaianjos@gmail.com>
2024-09-03 21:18:21 -0300

    Versão: 03-09-2024 - 21:18

--- plot.R
+++ plot.R
@@ -1,14 +1,24 @@
-# plot Pedro.F
+# Carregar pacotes necessários
+library(ggplot2)
+library(jsonlite)
 
-dados = read.csv(config$dataset)
-preds_json = fromJSON("local_de_saida/predicoes.json")
+source("config.R")
 
-ggplot(data = dados, aes(x = preds_treino, y = dados[[config$response_var]])) +
-  geom_point() +
-  geom_vline(xintercept = preds_json, linetype = "dashed", color = "red") +
-  ggtitle("Predições vs Valores Observados") +
-  xlab("Predições") +
-  ylab("Valores Observados") +
-  theme_minimal()
-
-ggsave("saidas/grafico.pdf")
+plotar_grafico = function() {
+  config = yaml::read_yaml(paste0(pasta_input, "/", arquivo_yaml))
+  
+  dados = read.csv(config$dataset)
+  preds_json = fromJSON("local_de_saida/predicoes.json")
+  
+  # Gerar o gráfico
+  grafico <- ggplot(data = dados, aes(x = preds_treino, y = dados[[config$response_var]])) +
+    geom_point() +  # Pontos dos valores preditos vs observados
+    geom_vline(xintercept = preds_json, linetype = "dashed", color = "red") +  # Linhas verticais para as predições do JSON
+    ggtitle("Predições vs Valores Observados") +  # Título do gráfico
+    xlab("Predições") +  # Rótulo do eixo x
+    ylab("Valores Observados") +  # Rótulo do eixo y
+    theme_minimal()  # Tema minimalista
+  
+  # Salvar o gráfico em PDF na pasta de saída
+  ggsave(paste0("saidas/grafico",config$tipo_modelo,".pdf"), plot = grafico)
+}
